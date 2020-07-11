2a263d09110df422c39afe302e6d6940b628584d
dmbuzink <dmbuzink@avans.nl>
2020-07-11 02:35:27 +0200

    init

---
+++ BlackDragonAI_Manager/BlackDragonAI_Manager/Models/CommandDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackDragonAI_Manager.Models
+{
+    public class CommandDetails
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "The command name is too long. Should be no longer than 255 characters.")]
+        [RegularExpression("![^ \\n]+", ErrorMessage = "Invalid command name")]
+        public string Command { get; set; }
+
+        [Required]
+        [StringLength(510, ErrorMessage = "The message is too long. Should be no longer than 510 characters.")]
+        public string Message { get; set; }
+        public string OriginalCommand { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(EPermission), ErrorMessage = "Invalid permission.")]
+        public EPermission Permission { get; set; } = EPermission.EVERYONE;
+        
+        [Required]
+        public uint Timer { get; set; } = 60;
+    }
+}
---
+++ BlackDragonAI_Manager/BlackDragonAI_Manager/Models/EPermission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackDragonAI_Manager.Models
+{
+    public enum EPermission
+    {
+        ADMIN = 0,
+        MODS = 1,
+        SUBS = 2,
+        EVERYONE = 3
+    }
+}
